7c62eb27ac078c6901153bf3084883623d0750bf
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-08 14:59:38 +0530

    made/ the start page.

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -4,7 +4,33 @@ export default function WelcomeScreen({ onStart }) {
   return (
     <div className={styles.container}>
       <div className={styles.options_holder}>
-
+          <div className={styles.notes}>
+            <h5 style={{color: '#478aff'}}>Meet your personal Waki-Talki</h5>
+            <h2 style={{color: '#7d63f3',marginBottom: '0.2em' }}><i><b>One-To-One</b></i></h2>
+            <h5 style={{letterSpacing: '0.01em',fontWeight: 400, color:'#334155'}}>
+              This app provides you a medium to connect with your friends, or people you trust
+              without relying on external servers.
+              <br/>
+              Means there is no probability that your data would be stored
+              in some corner of the internet.
+            </h5>
+            <h4 style={{color:'#155cc1',marginTop:'0.4em',marginBottom:'0.1em'}}>Usage Guide</h4>
+            <h5 style={{letterSpacing: '0.01em' , fontWeight: 400, color:'#080c11'}}>
+              1. You can create a chat or join a chat
+              <br/>
+              2. you can see a text, copy and send this to 
+              the trusted one you want to talk to.
+              <br/>
+              3. Now they will send his handshake key. you need to paste his key to initiate the handshake.
+              <br/>
+              4. And they also have to do the same process , paste your handshake key.
+            </h5>
+          </div>
+          <div className={styles.action_keys}>
+            <button className={styles.make_chat}><b>-Create-</b></button>
+            <button className={styles.make_chat}><b>-Join-</b></button>
+            <button className={styles.make_chat}><b>..Read..Dcos..</b></button>
+          </div>
       </div>
     </div>
   );
