9ce939b893c40763ecf4d37ac54595857f2ca17f
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-12 10:50:12 +0530

    made the chat incomeing and outgoing interface and layout.

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,7 +3,18 @@ import styles from './ChatBox.module.css'
 export default function ChatBox(){
     return(
         <div className={styles.chatbox}>
+            
+            <div className={styles.messages_in}>
+                <div className={styles.incomeing}>Hey Hiii</div>
+            </div>
 
+            <div className={styles.messages_out}>
+                <div className={styles.outgoing}>Hey</div>
+            </div>
+
+            <div className={styles.messages_out}>
+                <div className={styles.outgoing}>Hiiii There</div>
+            </div>
         </div>
     )
 }
