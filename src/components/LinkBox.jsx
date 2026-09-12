b75091b261c8ae08b8d247c398120db3c2b82516
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-13 00:06:11 +0530

    did some Ui modifications.

--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -10,20 +10,20 @@ export default function LinkBox({onStartChat}){
         <div className={styles.box}>
             <div className={styles.input_box}>
                 <div className={styles.token_name}>
-                    <h2 className={styles.token_name_text}>Your Token</h2>
+                    <h2 className={styles.token_name_text}>Your Token:</h2>
                 </div>
                 <textarea 
                 readOnly
-                className={styles.show_token}>
-
+                className={styles.show_my_token}>
+                
                 </textarea>
             </div>
             <div className={styles.input_box}>
                 <div className={styles.token_name}>
-                    <h2 className={styles.token_name_text}>Peer Token</h2>
+                    <h2 className={styles.token_name_text}>Peer Token:</h2>
                 </div>
                 <textarea               
-                className={styles.show_token}>
+                className={styles.take_token}>
                 </textarea>
             </div>
             <div className={styles.button_holder}>
--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -5,11 +5,13 @@ export default function WelcomeScreen({ connect, onReadDocs }){
     <div className={styles.container}>
       <div className={styles.options_holder}>
           <div className={styles.notes}>
-
+            
             <h5 style={{color: '#478aff'}}>Meet your personal Waki-Talki</h5>
             
             <h2 style={{color: '#7d63f3',marginBottom:'1cqh'}}><i><b>One-To-One</b></i></h2>
             
+            <img src="/AppIcon.svg" className={styles.icon}></img>
+
             <h5 style={{color:'#415168'}}>
               This app provides you a medium to connect with your friends, or people you trust
               without relying on external servers.
