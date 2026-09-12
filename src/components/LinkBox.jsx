5df2768a71d6c29324bc9db041b2f51a60515991
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-12 11:09:25 +0530

    fixed the linkbox styleings little bit...

--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -1,22 +1,29 @@
 import styles from './LinkBox.module.css'
 
 export default function LinkBox({onStartChat}){
+
+
+
+
+    
     return(
         <div className={styles.box}>
             <div className={styles.input_box}>
                 <div className={styles.token_name}>
-                    <h2>Your Token</h2>
+                    <h2 className={styles.token_name_text}>Your Token</h2>
                 </div>
-                <textarea className={styles.show_token}>
+                <textarea 
+                readOnly
+                className={styles.show_token}>
 
                 </textarea>
             </div>
             <div className={styles.input_box}>
                 <div className={styles.token_name}>
-                    <h2>Peer Token</h2>
+                    <h2 className={styles.token_name_text}>Peer Token</h2>
                 </div>
-                <textarea className={styles.show_token}>
-
+                <textarea               
+                className={styles.show_token}>
                 </textarea>
             </div>
             <div className={styles.button_holder}>
