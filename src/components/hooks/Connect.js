81185f06f412207cb3f2fa5ffcfa29d7fdef34a3
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-15 21:13:32 +0530

    written the code of connect.js

--- src/components/hooks/Connect.js
+++ src/components/hooks/Connect.js
@@ -1,4 +1,42 @@
 
-export function connectConnection() {
-    console.log("creating a connection...");
+
+let peerConnection;
+let notifyMessage;
+
+export async function connectConnection(onmessage) {
+    peerConnection = new RTCPeerConnection();
+    notifyMessage = onmessage
+
+    peerConnection.ondatachannel = (event) => {
+        const datachannel = event.channel;
+
+        datachannel.onmessage = (e) => {
+            const msg = JSON.parse(e.data);
+            const organized_msg = { type:"incomeing" , text:msg}
+            notifyMessage(organized_msg)
+        }
+    }
+}
+
+export async function acceptOffer(offer){
+    await peerConnection.setRemoteDescription(offer)
+
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    await iceGather(peerConnection)
+
+    return JSON.stringify(peerConnection.localDescription)
+}
+
+function iceGather(pc){
+    return new Promise((resolve)=>{
+        if(pc.iceGatheringState==="complete"){
+            resolve();
+        }else{
+            pc.onicegatheringstatechange = () => {
+                if (pc.iceGatheringState==="complete") resolve();
+            };
+        }
+    });
 }
