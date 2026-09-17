190468f36c0856aecfcf015d2e07cf2724f34cae
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-17 09:02:44 +0530

    the chatbox and the inputbox was showing scroolbars, made them display none, and also added enter to send messages, and edge case dictator, no message will send if the content is empty..

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -4,20 +4,6 @@ export default function ChatBox({message}){
 
     return(
         <div className={styles.chatbox}>
-            
-            <div className={styles.messages_in}>
-                <div className={styles.incomeing}>Hey Hiii</div>
-            </div>
-
-            <div className={styles.messages_out}>
-                <div className={styles.outgoing}>Hey</div>
-            </div>
-
-            <div className={styles.messages_out}>
-                <div className={styles.outgoing}>Hiiii There</div>
-            </div>
-
-
             {
                 message.map((msg)=>(
                     <div className={msg.type ==="outgoing" ? styles.messages_out : styles.messages_in}>
