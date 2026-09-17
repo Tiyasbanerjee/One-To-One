7bc956fb570120b3f0992b682f03b5bd52ff11b3
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-09-17 10:34:59 +0530

    updated the root path , because its showing 404 error.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative base because its single page site..
+  base: '/One-To-One/',
 });
